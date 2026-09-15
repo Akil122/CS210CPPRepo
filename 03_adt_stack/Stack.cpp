0a369be4e13f92700a206260c8079b55f43623cb
Akil Shaik <akilshaik@Akils-MacBook-Pro.local>
2026-09-14 22:42:04 -0700

    Implement Stack peek with empty check

--- 03_adt_stack/Stack.cpp
+++ 03_adt_stack/Stack.cpp
@@ -40,3 +40,12 @@ int Stack::pop() {
     topIndex--;
     return value;
 }
+
+int Stack::peek() const {
+    if (isEmpty()) {
+        std::cout << "Stack is empty." << std::endl;
+        return -1;
+    }
+
+    return data[topIndex];
+}
