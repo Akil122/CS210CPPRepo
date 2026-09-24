7cf2e037ed9f42e940047772715ad521a43ffc8d
Akil Shaik <akilshaik@Akils-MacBook-Pro.local>
2026-09-24 00:36:01 -0700

    Refactor Node to store data pointer

--- 12_list_refactor/Node.h
+++ 12_list_refactor/Node.h
@@ -1,22 +1,10 @@
-//
-// Created by Akil Shaik on 9/17/26.
-//
-
-# pragma once
-#include <iostream>
-#include <ostream>
+#pragma once
 
 template <typename T>
 class Node {
 public:
-    T:value;
-    Node<T> *next;
+    T* data;
+    Node<T>* next;
 
-    Node(T * value) {
-        this->value=value;
-        this->next=nullptr;
-    }
-    void print() {
-        cout<<value<<endl;
-    }
+    explicit Node(T* value) : data(value), next(nullptr) {}
 };
